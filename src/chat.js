21e3702cb3a7351150a4333c23c57aadd895c2ca
Shinya <halen_tft@yahoo.co.jp>
2026-05-01 20:35:35 +0900

    名前欄調整

--- src/chat.js
+++ src/chat.js
@@ -479,7 +479,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     //* 名前の文字数チェック（15文字まで）
-    if (name !== "名無し" && name.length > 15) {
+    if (name !== "名前は15文字以内です" && name.length > 15) {
       alert("名前は15文字以内にしてください。");
       return;
     }
